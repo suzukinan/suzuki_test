26dc2d44112310cc35a53ad289944bf66b5e44be
suzuki <tateos.study.only@gmail.com>
2019-07-31 20:05:26 +0900

    付録問題１

--- javaTest/src/javaTest/Test1.java
+++ javaTest/src/javaTest/Test1.java
@@ -1,10 +1,18 @@
 package javaTest;
-
+import java.util.Scanner;
 public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		Scanner scan = new Scanner(System.in);
 
+		String a = scan.next();
+		String b = scan.next();
+		String c =  a + b;
+		System.out.println("a+b"+ c +"です");
+		c = c + 1;
+		System.out.println("c+1=" + c + "です");
+
+		scan.close();
 	}
-
 }
