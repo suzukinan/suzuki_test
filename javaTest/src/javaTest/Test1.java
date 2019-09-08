db1a5698bc54dab30236b1932a1fbde551a8777d
suzuki <tateos.study.only@gmail.com>
2019-09-08 13:31:25 +0900

    ジャンケン課題

--- javaTest/src/javaTest/Test1.java
+++ javaTest/src/javaTest/Test1.java
@@ -6,12 +6,29 @@ public class Test1 {
 		// TODO 自動生成されたメソッド・スタブ
 		Scanner scan = new Scanner(System.in);
 
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c =  a + b;
-		System.out.println("a+b=" + c + "です");
-		c = c + 1;
-		System.out.println("c+1=" + c + "です");
+        // 以下は問題１
+//		int a = scan.nextInt();
+//		int b = scan.nextInt();
+//		int c =  a + b;
+//		System.out.println("a+b=" + c + "です");
+//		c = c + 1;
+//		System.out.println("c+1=" + c + "です");
+
+
+
+		// 以下は問題２
+		int nenrei = scan.nextInt();
+		int kakaku = 0;
+
+		if (nenrei == 0) {
+			kakaku = 100;
+
+		}else {
+			kakaku = 500;
+		}
+		System.out.println("年齢が" + nenrei + "歳のとき価格は" + kakaku + "円です。");
+
+
 
 		scan.close();
 	}
