61b03ca3e89dbe88255fbe4a9a18a66b56b04c3e
suzuki <tateos.study.only@gmail.com>
2019-09-09 23:02:39 +0900

    課題修正

--- javaTest/src/javaTest/Janken.java
+++ javaTest/src/javaTest/Janken.java
@@ -5,17 +5,24 @@ public class Janken {
 		public static void main(String[] args) {
 			Scanner scan = new Scanner(System.in);
 
+//			player1に選択させる
 			System.out.println("player1 選択して下さい。");
 			System.out.println("1:グー、2:チョキ、3:パー");
 			int player1 = scan.nextInt();
 
+//			player2に選択させる
 			System.out.println("player2 選択して下さい。");
 			System.out.println("1:グー、2:チョキ、3:パー");
 			int player2 = scan.nextInt();
 
+//			配列に入れる
+			String[] jankens = {"","グー","チョキ","パー"};
+			System.out.println(jankens[player1] + " VS " +jankens[player2]);
+
+//			条件分岐
 			if (player1 == player2) {
 				System.out.println("あいこです");
-			}else if ((player1 == 0 && player2 == 1) || (player1 == 1 && player2 == 2) || (player1 == 2 && player2 == 0)){
+			}else if ((player1 == 1 && player2 == 2) || (player1 == 2 && player2 == 3) || (player1 == 3 && player2 == 1)){
 				System.out.println("player1の勝ちです");
 				System.out.println("player2の負けです");
 			}else {
